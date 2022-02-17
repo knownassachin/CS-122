6ebf8f49dc8a2d5da92844cb863ec69907f40d9e
knownassachin <98410916+knownassachin@users.noreply.github.com>
2022-02-17 14:12:24 -0500

    Update Sports.java

--- CS122/src/classSamples/polymorphism/SearchingAndSorting/Sports.java
+++ CS122/src/classSamples/polymorphism/SearchingAndSorting/Sports.java
@@ -1,44 +1,43 @@
 package classSamples.polymorphism.SearchingAndSorting;
 
-public class Sports implements Comparable<Sports>{
+public class Sports implements Comparable<Sports> {
 	private final String name;
 	private final int players;
-	
+
 	public Sports(String name, int players) {
 		this.name = name;
 		this.players = players;
 	}
-	
+
 	public int getPlayers() {
 		return players;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String toString() {
 		return name + " " + players + "\n";
 	}
-	
+
 	@Override
-	public boolean equals(Object other) {
-		return (name.equals(((Sports) other).getName()));
+	public boolean equals(Object o) {
+		return this.name.equals(((Sports) o).getName());
 	}
+
 	@Override
 	public int compareTo(Sports o) {
-		int result;
-
-		if (name.equals(o.getName()))
-			result = this.name.compareTo(o.getName());
-		return 0;
+		return this.name.compareTo(o.getName());
 	}
+
 	public static void main(String[] args) {
 		Sports[] rulemSports = new Sports[3];
 
-		rulemSports[0] = new Sports("John", "Smith", "610-555-7384");
-		rulemSports[1] = new Sports("Sarah", "Barnes", "215-555-3827");
-		rulemSports[2] = new Sports("Mark", "Riley", "733-555-2969");
-		
+		rulemSports[0] = new Sports("Baseball", 1);
+		rulemSports[1] = new Sports("Basketball", 8);
+		rulemSports[2] = new Sports("Rowing", 9);
+
 		Sorting<Sports> sorts = new Sorting<Sports>();
 		sorts.selectionSort(rulemSports);
 
