42cf18e5edfa8d21154cc0f298d3229922b60755
knownassachin <98410916+knownassachin@users.noreply.github.com>
2022-04-07 21:18:09 -0400

    midterm

---
+++ CS122/src/midterm2Review/ExceptionRe.java
@@ -0,0 +1,10 @@
+package midterm2Review;
+
+public class ExceptionRe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CS122/src/midterm2Review/RecursionRe.java
@@ -0,0 +1,10 @@
+package midterm2Review;
+
+public class RecursionRe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
